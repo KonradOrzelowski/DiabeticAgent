55157ffb3d23903abfbe8cbcda1c17b6bd7493da
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-03-28 15:34:19 +0100

    Init get_stats_for_day.py

---
+++ mongo_analysis/get_stats_for_day.py
@@ -0,0 +1,69 @@
+import os
+import json
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import pandas as pd
+from bson import json_util
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from utils.get_sgv import get_sgv
+from utils.get_temp_basal import get_temp_basal
+# Load environment variables from .env
+load_dotenv()
+
+# Get MongoDB credentials from environment variables
+MONGO_HOST = os.getenv("HOST_NAME")
+MONGO_PORT = int(os.getenv("MONGO_PORT"))
+MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+# MongoDB URI
+MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/"
+date = '2023-12-31'
+
+client = MongoClient(MONGO_URI)
+db = client["diabetic_records"]
+
+collections = db.list_collection_names()
+
+collection = db['Entries']
+
+documents = collection.aggregate([
+  {
+    '$addFields': {
+      'dateString': { '$toDate': '$dateString' }
+    }
+  },
+  {
+    '$project': {
+      'dateString': '$dateString',
+      'newField': {  
+        '$concat': [
+          {'$toString': { '$year': '$dateString' }}, '-',
+          {'$cond': [
+              { '$lt': [{'$month': '$dateString'}, 10] },  # If the month is less than 10
+              {'$concat': ['0', {'$toString': { '$month': '$dateString' }}]},  # Add leading zero
+              {'$toString': { '$month': '$dateString' }}  # Otherwise, just the month as is
+            ]}, '-',
+          {'$cond': [
+              { '$lt': [{'$dayOfMonth': '$dateString'}, 10] },  # If the day is less than 10
+              {'$concat': ['0', {'$toString': { '$dayOfMonth': '$dateString' }}]},  # Add leading zero
+              {'$toString': { '$dayOfMonth': '$dateString' }}  # Otherwise, just the day as is
+            ]}
+        ]
+      }
+    }
+  },
+  { '$group': { '_id': "$newField" } }
+])
+
+data = list(documents)
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
