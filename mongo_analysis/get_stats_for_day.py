cc87da12210c529564efd5d9282383dcce25da93
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-03-29 13:10:33 +0100

    Remove old files

--- mongo_analysis/get_stats_for_day.py
+++ mongo_analysis/get_stats_for_day.py
@@ -1,15 +1,12 @@
-from metric import calculate_summary
-from plot_glucose_data import plot_glucose_data
-
 from utils.get_readings import getReadings
 from utils.glucose_analysis import GlucoseAnalysis
 
+from utils.plot_glucose_data import plot_glucose_data
 
 date = '2023-12-31'
 
 # Load dataframes
 gr = getReadings()
-
 
 insulin_carbs = gr.get_insulin_carbs(date)
 sugar_values = gr.get_sgv(date)
@@ -19,7 +16,7 @@ bolus_wizard = gr.get_bolus_wizard(date)
 
 ga = GlucoseAnalysis(sugar_values)
 
-for key, val in ga.calculate_summary(sugar_values).items():
+for key, val in ga.calculate_summary().items():
     print(f'{key}: {val}')
 
 plot_glucose_data(date, temp_basal, insulin_carbs, sugar_values)
