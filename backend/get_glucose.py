4221473d407ddda161dc2cce36771a0d970ab30f
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-04-26 17:15:38 +0200

    Add get_night_stats

---
+++ backend/get_glucose.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from pymongo import MongoClient
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+from utils.get_readings import getReadings
+
+
+DEFAULT_TIMEZONE_OFFSET_HOURS = 5
+DEFAULT_COLLECTION_NAME = 'Entries'
+
+
+
+def get_night_stats(end_day: str, time_range_days: int, last_night_hour: int) -> pd.DataFrame:
+    """
+    Computes nightly statistics of glucose readings between specific hours over a given date range.
+
+    Args:
+        end_day (str): The end date in 'YYYY-MM-DD' format.
+        time_range_days (int): How many days back to consider.
+        last_night_hour (int): Last hour considered as 'night' (e.g., 7 for 7 AM).
+
+    Returns:
+        pd.DataFrame: Nighttime statistics (min, max, mean, quartiles) grouped by date.
+    """
+
+    gt = getReadings()
+
+    start_day = pd.Timestamp(end_day) - pd.Timedelta(time_range_days, "d")
+
+
+    t_start_day = pd.Timestamp(start_day) - pd.Timedelta(DEFAULT_TIMEZONE_OFFSET_HOURS, "h")
+    t_end_day = pd.Timestamp(end_day) + pd.Timedelta(DEFAULT_TIMEZONE_OFFSET_HOURS, "h")
+
+
+    query = {
+        'sgv': {
+            '$exists': True
+        },
+        'dateString': {
+            '$gte': t_start_day.isoformat(),
+            '$lte': t_end_day.isoformat()
+        }
+    }
+    df = gt.get_data_from_collection(DEFAULT_COLLECTION_NAME, query)
+    if df.empty:
+        return pd.DataFrame()
+
+    df['date'] = df['created_at'].dt.date
+
+    df = df.set_index('created_at')
+    df_night_hours = df.between_time('00:00', f'{last_night_hour}:00')
+
+
+
+
+    stats_night_hours = df_night_hours.groupby(['date'])['sgv'].agg([
+        ('min', 'min'),
+        ('max', 'max'),
+        ('mean', 'mean'),
+        ('25th_per', lambda x: x.quantile(0.25)),
+        ('50th_per', lambda x: x.quantile(0.5)),
+        ('75th_per', lambda x: x.quantile(0.75))
+    ]).round(2)
+
+    return stats_night_hours
+
+end_day = '2024-10-08'
+time_range_days = 14
+last_night_hour = 7
+df = get_night_stats(end_day, time_range_days, last_night_hour)
+
+print(df.to_markdown())
---
+++ backend/utils/get_readings.py
@@ -0,0 +1,127 @@
+import os
+
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+class getReadings:
+    def __init__(self, database = "diabetic_records"):
+
+        self.database = database
+
+        self.MONGO_HOST = os.getenv("HOST_NAME")
+        self.MONGO_PORT = int(os.getenv("MONGO_PORT"))
+        self.MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+        self.MONGO_PASS = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+
+
+        self.MONGO_URI = f"mongodb://{self.MONGO_USER}:{self.MONGO_PASS}@{self.MONGO_HOST}:{self.MONGO_PORT}/"
+
+        # Connect to MongoDB
+        try:
+            self.client = MongoClient(self.MONGO_URI, serverSelectionTimeoutMS=5000)
+            self.db = self.client[self.database]
+        except Exception as e:
+            print(f"Could not connect to MongoDB: {e}")
+
+
+
+    def get_data_from_collection(self, collection_name, query, sort_by='created_at'):
+        """
+        Helper function to retrieve data from a MongoDB collection and return it as a pandas DataFrame.
+        
+        Parameters:
+            collection_name (str): The name of the collection to query.
+            query (dict): The query to execute.
+            sort_by (str): The field to sort the results by (default is 'created_at').
+        
+        Returns:
+            pd.DataFrame: A DataFrame containing the query results sorted by the specified field.
+        """
+        collection = self.db[collection_name]
+        documents = collection.find(query).sort(sort_by, 1)  # 1 for ascending order
+        data = list(documents)
+        
+        # Convert to DataFrame
+        df = pd.DataFrame(data)
+
+        has_created_at = 'created_at' in df.columns
+        has_dateString = 'date' in df.columns
+        
+        # Convert 'created_at' to datetime and sort
+        if has_created_at:
+            df['created_at'] = pd.to_datetime(df['created_at'])
+            
+        elif not has_created_at and has_dateString:
+            
+            df['created_at'] = pd.to_datetime(df["date"], unit="ms").dt.tz_localize('UTC')
+
+        # else:
+        #     raise ValueError('No time column!')
+
+        # df[sort_by] = df[sort_by].to_timestamp()
+
+        df.sort_values(by=sort_by, inplace=True)
+
+        return df
+
+
+    def get_bolus_wizard(self, date):
+
+        collection_name = 'Treatments'
+
+        query = {
+            'eventType': 'Bolus Wizard',
+            'created_at': {'$regex': f'^{date}'}
+        }
+
+        return self.get_data_from_collection(collection_name, query)
+
+
+
+    def get_insulin_carbs(self, date):
+
+        collection_name = 'Treatments'
+
+        query = {
+            '$or': [ 
+                { 'insulin': {'$exists': True} }, 
+                { 'carbs': {'$exists': True} }
+            ],
+            'created_at': {'$regex': f'^{date}'}
+        }
+
+        return self.get_data_from_collection(collection_name, query)
+
+    def get_sgv(self, date):
+
+        collection_name = 'Entries'
+
+        query = {
+            'sgv': {'$exists': True},
+            'dateString': {'$regex': f'^{date}'}
+        }
+
+        return self.get_data_from_collection(collection_name, query)
+
+
+    def get_temp_basal(self, date):
+
+        collection_name = 'Treatments'
+
+        query = {
+            'rate': {'$exists': True},
+            'created_at': {'$regex': f'^{date}'}
+        }
+
+        return self.get_data_from_collection(collection_name, query)
+
+    
+    def get_carbs_info(self, date, time_diff = 5):
+        pass
