8f5f22bbc458f354f7640c7bd39e31865f6cbf09
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-05-06 21:13:15 +0200

    Remove old process_pdf

--- backend/load_vectorstore.py
+++ backend/load_vectorstore.py
@@ -1,23 +1,45 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from process_pdf import ProcessPDF
 
-embeddings = OpenAIEmbeddings()
+def load_faiss_index(file_name):
+    pp = ProcessPDF(file_name)
+    pp.load_vectorestore()
+    return pp.vectorstore
 
-new_vector_store = FAISS.load_local(
-    "faiss_index", embeddings, allow_dangerous_deserialization=True
-)
+faiss_index = load_faiss_index('dr_bernstein_diabetes_solution')
+faiss_index_1 = load_faiss_index('standards_of_care_2025')
 
-docs = new_vector_store.similarity_search("High sugar values during fasting")
+faiss_index.merge_from(faiss_index_1)
+
+question = "Should I give insulin before I eat? Something like 10 or 20 minutes before a meal?"
+
+docs = faiss_index.similarity_search(question)
+doc_content = {}
 
 for idx, doc in enumerate(docs):
-    # Split by \n
+    doc_content[f'doc_{idx}'] = doc.page_content
     print(f"{f'Doc {idx}':-^50}")
-    lines = doc.page_content .split('\n')
+    lines = doc.page_content.split('\n')
 
-    # Print each line separately
+    # Print each non-empty line
     for line in lines:
-        if line.strip():  # Skip empty lines
+        if line.strip():
             print(line)
 
+messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are a diabetic assistant. "
+            "Your role is to assist users in better managing type 1 diabetes. "
+            "You may reference the documents provided to answer questions."
+        )
+    },
+    {
+        "role": "user",
+        "content": f"Helpful documents:\n\n{doc_content}\n\nPlease respond to the following question:\n\n{question}"
+    }
+]
 
-
+print(messages)
--- backend/pdf_to_txt_pipeline.py
+++ backend/pdf_to_txt_pipeline.py
@@ -1,41 +1,9 @@
 import os
 import json
-from process_pdf.extract_pdf_to_text import extract_pdf_to_text
-from process_pdf.clean_text import clean_text
-from process_pdf.split_text_into_chunks import split_text_into_chunks
-from process_pdf.format_chunks import format_chunks
-
-def standards_of_care_2025():
-    file_name = 'standards-of-care-2025'
-    output_dir = os.path.join('output', file_name)
-    os.makedirs(output_dir, exist_ok=True)
-
-    extract_pdf_to_text(file_name=file_name)
-
-    text_path = os.path.join(output_dir, f'{file_name}.txt')
-    with open(text_path, 'r', encoding='utf-8') as f:
-        text = f.read()
-
-    text = clean_text(text)
-
-    processed_text_path = os.path.join(output_dir, f'{file_name}_processed.txt')
-    with open(processed_text_path, 'w', encoding='utf-8') as f:
-        f.write(text)
-
-    raw_chunks = split_text_into_chunks(text)
-
-    raw_chunks_path = os.path.join(output_dir, f'{file_name}_chunks.json')
-    with open(raw_chunks_path, 'w', encoding='utf-8') as f:
-        json.dump(raw_chunks, f, indent=4)
-
-    formatted_chunks = format_chunks(raw_chunks)
-
-    formatted_chunks_path = os.path.join(output_dir, f'{file_name}_chunks-formatted.json')
-    with open(formatted_chunks_path, 'w', encoding='utf-8') as f:
-        json.dump(formatted_chunks, f, indent=4)
 
 
-from process_pdf_ import ProcessPDF
+
+from process_pdf import ProcessPDF
 
 file_name = 'standards_of_care_2025'
 pp = ProcessPDF(file_name)
