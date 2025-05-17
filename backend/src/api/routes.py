5052143f18f33ff47ef57c027ff07cfcdbc424cc
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-05-17 15:04:58 +0200

    Start api endpoints

---
+++ backend/src/api/routes.py
@@ -0,0 +1,61 @@
+from typing import Union
+from fastapi import FastAPI
+
+from agents.get_agent import Agent
+
+
+app = FastAPI()
+
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from processing.process_pdf import ProcessPDF
+from langchain.agents import initialize_agent, Tool
+from langchain_core.tools import tool
+from agents.get_agent import Agent
+
+# Load and merge FAISS indices
+def load_faiss_index(file_name):
+    pp = ProcessPDF(file_name)
+    pp.load_vectorestore()
+    return pp.vectorstore
+
+# Define tool for document search
+@tool("Diabetes Document Search", parse_docstring=True)
+def get_relevent_docs(question: str, extended: bool = False) -> str:
+    """
+    Retrieve and format relevant documents from a FAISS index based on a given question.
+
+    Args:
+        question (str): The input question used to perform a similarity search in the FAISS index.
+
+    Returns:
+        str: A formatted string containing the contents of the most relevant documents, each labeled by index.
+    """
+    docs = faiss_index.similarity_search(question)
+    doc_content = {}
+    new_line = '\n'
+
+    for idx, doc in enumerate(docs):
+        doc_content[f'doc_{idx}'] = doc.page_content
+
+    docs_str = ''.join([f"{key}: {value}{new_line}" for key, value in doc_content.items()])
+
+    return docs if extended else docs_str
+
+@app.get("/")
+def read_root():
+    query = 'Hi this is test'
+
+    init_agent = Agent("gpt-4o-mini", tools=[get_relevent_docs])
+    agent = init_agent.agent_with_chat_history
+    config = {"configurable": {"session_id": init_agent.session_id}}
+
+    response = agent.invoke({'input': query}, config)
+
+
+    return {"response": response}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
