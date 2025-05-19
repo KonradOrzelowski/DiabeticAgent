3f919dcca8db9cba49741427b972ea919e9efda6
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2025-05-19 21:21:32 +0200

    Add input to html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,38 +4,36 @@ import Axios from "axios";
 
 import ReactMarkdown from 'react-markdown';
 
+
+
 function App() {
     const [products, setProducts] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [pressEnter, setPressEnter] = useState(false);
+
 
     const fetchProducts = async () => {
         const { data } = await Axios.get("http://127.0.0.1:8000/response/");
         const products = data;
         setProducts(products);
     };
+    
 
     useEffect(() => {
         fetchProducts();
-    }, []);
+        console.log('!!!!!!!!!!!!!!!!')
+    }, [pressEnter]);
 
     try {
         const response = products.response;
 
-        const input = response.input;
         const output = response.output;
         const intermediate_steps = response.intermediate_steps || [];
-
-        console.log('--- Response Logging Start ---');
-        console.log('Input:', input);
-        console.log('Output:', output);
-        console.log('Intermediate Steps:', intermediate_steps[0][1]);
-        console.log('--- Response Logging End ---');
 
         const intermediateElements = [];
 
         for (let doc of intermediate_steps[0][1]) {
-            console.log(doc)
             intermediateElements.push(
-
                 <div>
                     <h3>{doc.metadata.origin_title} chunk number {doc.metadata.chunk_number}</h3>
                     <ReactMarkdown>{doc['page_content']}</ReactMarkdown>
@@ -43,18 +41,33 @@ function App() {
             );
         }
 
-        console.log(intermediateElements);
         return (
-            <div className="response">
+            <div>
+                <div className="response">
 
-                <div className="output">
-                    <ReactMarkdown>{output}</ReactMarkdown>
+                    <div className="output">
+                        <ReactMarkdown>{output}</ReactMarkdown>
+                    </div>
+
+                    <div className="docs">
+                        {intermediateElements}
+                    </div>
                 </div>
 
-                <div className="docs">
-                    {intermediateElements}
-                </div>
-                
+                <input
+                    type="text"
+                    placeholder="Type your query..."
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            alert(inputValue);
+                            setInputValue('');
+                            setPressEnter(~pressEnter)
+                        }
+                    }}
+                    className="input-box"
+                />
             </div>
         );
 
